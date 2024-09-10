e880d51c5d3959bb0ebd20e3d7f4d726fc1257dd
Rishav Dhungel <rishavdhungel1@gmail.com>
2024-09-10 20:58:23 +0545

    feat: complete functions for Rock, Paper, Scissors game
    - Added handling for all game outcomes: win, lose, tie
    - Included validation for invalid choices
    - Ensured proper function calls for getting choices

---
+++ script.js
@@ -0,0 +1,51 @@
+//create list of choices
+const choice = ['scissor','paper','rock'];
+let playerScore = 0;
+let computerScore = 0;
+
+
+function getComputerChoice(){
+    //return random computer choice
+    let choiceNumber =  Math.floor(Math.random()*3);
+    return choice[choiceNumber];
+}
+
+
+//get human choice using prompt
+function getHumanChoice(){
+    let humanchoice = prompt("Select 'scissor' 'paper' or 'rock");
+    return humanchoice.toLocaleLowerCase();
+}
+
+
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    } else if (humanChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            console.log("You lose! Paper beats Rock");
+        } else if (computerChoice === 'scissors') {
+            console.log("You win! Rock beats Scissors");
+        }
+    } else if (humanChoice === 'paper') {
+        if (computerChoice === 'rock') {
+            console.log("You win! Paper beats Rock");
+        } else if (computerChoice === 'scissors') {
+            console.log("You lose! Scissors beats Paper");
+        }
+    } else if (humanChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            console.log("You lose! Rock beats Scissors");
+        } else if (computerChoice === 'paper') {
+            console.log("You win! Scissors beats Paper");
+        }
+    } else {
+        console.log("Invalid choice. Please choose 'rock', 'paper', or 'scissors'.");
+    }
+}
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+console.log("Human Choice: "+ humanChoice);
+console.log("Computer Choice: "+ computerChoice);
+playRound(humanChoice,computerChoice);
